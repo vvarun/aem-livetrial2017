9acf9bfb1d6b1d33d56478872c4eff6117589e1d
vvenkata <vvenkata@adobe.com>
2017-06-16 09:55:51 +1000

    Working Sling Model exporter
    http://localhost:4502/content/we-retail/language-masters/en/men.model.json

--- core/src/main/java/com/adobe/livetrials/models/LivetrialModel.java
+++ core/src/main/java/com/adobe/livetrials/models/LivetrialModel.java
@@ -1,42 +1,43 @@
 package com.adobe.livetrials.models;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
 
-@Model(adaptables = Resource.class, resourceType = "myco/components/foo")
+@Model(adaptables = Resource.class, resourceType = "cq:Page")
 @Exporter(name = "jackson", extensions = "json")
 public class LivetrialModel {
 	
-    @Inject
+    @Inject @Named("jcr:content/jcr:title")
     private String pageTitle;
     
-    @Inject
-    private String pageDesc;
-    
-    @Inject
-    private String createDate;
-    
-    @Inject
-    private String noOfChildPages;   
+//    @Inject
+//    private String pageDesc;
+//    
+//    @Inject
+//    private String createDate;
+//    
+//    @Inject
+//    private String noOfChildPages;   
 
 	public String getPageTitle() {
 		return pageTitle;
 	}
-
-	public String getPageDesc() {
-		return pageDesc;
-	}
-
-	public String getCreateDate() {
-		return createDate;
-	}
-
-	public String getNoOfChildPages() {
-		return noOfChildPages;
-	}
+//
+//	public String getPageDesc() {
+//		return pageDesc;
+//	}
+//
+//	public String getCreateDate() {
+//		return createDate;
+//	}
+//
+//	public String getNoOfChildPages() {
+//		return noOfChildPages;
+//	}
 
 
 }
